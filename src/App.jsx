4e7ca5fe3ed4651ad8ad37553cdccf089b06abf3
AkoshBTC024242 <91231275+AkoshBTC024242@users.noreply.github.com>
2025-12-16 21:56:55 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import Properties from './pages/Properties'
-import About from './pages/About'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Properties from './pages/Properties';
+import About from './pages/About';
+import PropertyDetails from './pages/PropertyDetails';
 
-export default function App() {
+function App() {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/properties" element={<Properties />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-      <Footer />
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/properties" element={<Properties />} />
+            <Route path="/properties/:id" element={<PropertyDetails />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
-  )
+  );
 }
+
+export default App;
